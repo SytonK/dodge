cd7efc5c7eb2539e0139aa747590efb9931ff70d
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-09 23:57:07 +0300

    Initial commit

---
+++ player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+@export var movement_speed: float = 500
+
+
+func _physics_process(delta: float) -> void:
+	_move()
+	move_and_slide()
+
+
+func _move() -> void:
+	velocity = Vector2(
+			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+			Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	).normalized() * movement_speed
