9f1182ea4c4bae977069a76657e74d2aa2a5859e
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-12 22:37:06 +0300

    add bullet spawner frequency

--- enemies/global enemies/bullet target player/global_bullet_target_player.gd
+++ enemies/global enemies/bullet target player/global_bullet_target_player.gd
@@ -2,3 +2,18 @@ extends Node2D
 
 @export var speed: float
 @export var frequency: float
+@onready var timer: Timer
+
+
+func _ready() -> void:
+	_init_timer()
+
+func _init_timer() -> void:
+	timer = Timer.new()
+	timer.wait_time = frequency
+	timer.timeout.connect(_spawn_bullet)
+	timer.autostart = true
+	add_child(timer)
+
+func _spawn_bullet() -> void:
+	print('fire')
