6e8000382f99029b0145e6aa748e84790629e2ee
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-13 15:18:47 +0300

    add level ui

--- levels/level.gd
+++ levels/level.gd
@@ -5,6 +5,10 @@ extends Node2D
 const PLAYER = preload("res://player/player.tscn")
 var player: Player
 
+const LEVEL_UI = preload("res://UI/level UI/level_ui.tscn")
+var game_over_menu: Control
+var pause_menu: Control
+
 
 func _ready() -> void:
 	_add_player()
@@ -12,3 +16,8 @@ func _ready() -> void:
 func _add_player() -> void:
 	player = PLAYER.instantiate()
 	add_child(player)
+
+func _add_level_ui() -> void:
+	add_child(LEVEL_UI.instantiate())
+	game_over_menu = $LevelUI/GameOverMenu
+	pause_menu = $LevelUI/PauseMenu
