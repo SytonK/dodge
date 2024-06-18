244f73dbe5ae42525dd8a50e577ebab05ca391c3
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-18 20:13:24 +0300

    init normal lvl 1

--- levels/level 1/level_1.gd
+++ levels/level 1/level_1.gd
@@ -1,1 +1,11 @@
 extends Level
+
+
+@onready var global_bullet_parallal_direction: GlobalBulletParallalDirection = $GlobalBulletParallalDirection
+@onready var global_bullet_target_player: GlobalBulletTargetPlayer = $GlobalBulletTargetPlayer
+
+
+func _to_normal() -> void:
+	super._to_normal()
+	global_bullet_parallal_direction.frequency = 0.25
+	global_bullet_target_player.disabled = false
