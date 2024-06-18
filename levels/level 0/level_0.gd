61abe7a4c294856dd69aa4903a9dff27ef55112f
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-18 09:21:14 +0300

    wait to enable 2nd tower lvl 0

--- levels/level 0/level_0.gd
+++ levels/level 0/level_0.gd
@@ -10,4 +10,5 @@ func _to_normal() -> void:
 	super._to_normal()
 	global_bullet_target_player.frequency = 0.35
 	target_player_tower_left.disabled = false
+	await get_tree().create_timer(0.5).timeout
 	target_player_tower_right.disabled = false
