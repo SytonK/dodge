5ff4e02dcad19fa4be759081c3cf7a9c3d8f4c50
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-18 21:58:23 +0300

    lvl1

--- levels/level 1/level_1.gd
+++ levels/level 1/level_1.gd
@@ -6,8 +6,20 @@ extends Level
 @onready var parallal_tower: ParallalTower = $ParallalTower
 
 
+func _ready() -> void:
+	super._ready()
+	player.position = Vector2(0, 150)
+
+
 func _to_normal() -> void:
 	super._to_normal()
 	global_bullet_parallal_direction.frequency = 0.25
 	global_bullet_target_player.disabled = false
 	parallal_tower.disabled = false
+
+func _to_hard() -> void:
+	super._to_hard()
+	global_bullet_parallal_direction.frequency = 0.1
+	global_bullet_target_player.speed = 700
+	parallal_tower.frequency = 0.45
+	parallal_tower.speed = 550
