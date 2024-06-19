a4abdab783f5881335402309412709117ff57247
SytonK <syton98k@gmail.com>
2024-06-19 14:05:36 +0300

    lvl 2

--- src/levels/level 2/level_2.gd
+++ src/levels/level 2/level_2.gd
@@ -1,1 +1,15 @@
 extends Level
+
+
+@onready var chaser: Chaser = $chaser
+@onready var global_bullet_parallal_direction: GlobalBulletParallalDirection = $GlobalBulletParallalDirection
+@onready var global_bullet_target_player: GlobalBulletTargetPlayer = $GlobalBulletTargetPlayer
+
+
+func _to_normal() -> void:
+	super._to_normal()
+	chaser.movement_speed = 300
+	global_bullet_parallal_direction.frequency = 0.4
+	global_bullet_parallal_direction.speed = 400
+	global_bullet_target_player.frequency = 1
+	
