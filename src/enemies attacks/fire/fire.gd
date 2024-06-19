667d0bc9a0f0528a296e55ccc86dfccfdf6aa10b
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-20 00:46:22 +0300

    add fire lifetime

---
+++ src/enemies attacks/fire/fire.gd
@@ -0,0 +1,11 @@
+extends Hitbox
+
+
+@export var lifetime: float
+var curr_lifetime: float = 0
+
+
+func _process(delta: float) -> void:
+	curr_lifetime += delta
+	if curr_lifetime > lifetime:
+		queue_free()
