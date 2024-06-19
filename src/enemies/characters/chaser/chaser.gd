034c88b8f36c970665675b1cf0c5ed3aff30bd1c
SytonK <syton98k@gmail.com>
2024-06-19 10:36:47 +0300

    chaser recovery

--- src/enemies/characters/chaser/chaser.gd
+++ src/enemies/characters/chaser/chaser.gd
@@ -4,11 +4,19 @@ extends CharacterBody2D
 
 @export var movement_speed: float
 
+@export var is_recovering: bool = false
+
+@onready var animation_player = $AnimationPlayer
 
 func _process(_delta: float) -> void:
-	_chase_player()
+	if !is_recovering:
+		_chase_player()
 
 func _chase_player() -> void:
 	velocity = (PlayerRef.player_ref.position - position).normalized() * movement_speed
 	rotation = velocity.angle()
 	move_and_slide()
+
+
+func _on_hitbox_hit():
+	animation_player.play("recover")
