397d766b251b3acc53b4ab3b8184ea128972bbc4
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-20 00:56:56 +0300

    add fire trail

--- src/enemies/characters/chaser/chaser.gd
+++ src/enemies/characters/chaser/chaser.gd
@@ -5,8 +5,11 @@ extends CharacterBody2D
 @export var movement_speed: float
 
 @export var is_recovering: bool = false
-
 @onready var animation_player = $AnimationPlayer
+
+const FIRE = preload("res://src/enemies attacks/fire/fire.tscn")
+@export var leavse_fire_trail: bool = false
+
 
 func _process(_delta: float) -> void:
 	if !is_recovering:
@@ -20,3 +23,16 @@ func _chase_player() -> void:
 
 func _on_hitbox_hit():
 	animation_player.play("recover")
+
+
+func _leave_fire_trial() -> void:
+	var new_fire: Fire = FIRE.instantiate()
+	new_fire.top_level = true
+	new_fire.lifetime = 2
+	new_fire.position = position
+	add_child(new_fire)
+
+
+func _on_firetrail_timer_timeout() -> void:
+	if leavse_fire_trail:
+		_leave_fire_trial()
