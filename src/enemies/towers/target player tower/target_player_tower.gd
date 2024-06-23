eaf78ed6b1761e551a706f65073197a7465d8761
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-24 00:05:43 +0300

    target player tower

--- src/enemies/towers/target player tower/target_player_tower.gd
+++ src/enemies/towers/target player tower/target_player_tower.gd
@@ -11,6 +11,8 @@ const BULLET = preload("res://src/enemies attacks/projectiles/bullet/bullet.tscn
 @export var frequency: float
 @onready var timer: Timer
 
+var bullet_color: Color = Color(1, 0, 0)
+
 
 func _ready() -> void:
 	_init_timer()
@@ -39,12 +41,12 @@ func _spawn_bullet() -> void:
 	var new_bullet = BULLET.instantiate()
 	new_bullet.movment_speed = speed
 	new_bullet.direction = PlayerRef.player_ref.global_position - global_position
-	new_bullet.modulate = Color(
-		0.9,
-		0.9 - Colors.MODULATE_FACTOR * speed,
-		0.9 - Colors.MODULATE_FACTOR * speed
-	)
+	new_bullet.modulate = _get_bullet_color(new_bullet)
 	add_child(new_bullet)
+
+func _get_bullet_color(new_bullet: Bullet) -> Color:
+	return Color(bullet_color * ((600 + new_bullet.movment_speed) / 1800), 1)
+
 
 func _set_frequency(new_frequency: float) -> void:
 	frequency = new_frequency
