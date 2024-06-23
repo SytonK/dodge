4f0afbf58861900cde0d85d421c9c0ec9b5b841e
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-24 02:16:50 +0300

    lvl 2 fixs

--- src/levels/level 2/level_2.gd
+++ src/levels/level 2/level_2.gd
@@ -2,8 +2,8 @@ extends Level
 
 
 @onready var chaser: Chaser = $chaser
-@onready var global_bullet_parallal_direction: GlobalBulletParallalDirection = $GlobalBulletParallalDirection
-@onready var global_bullet_target_player: GlobalBulletTargetPlayer = $GlobalBulletTargetPlayer
+@onready var global_bullet_spawner_parallal_direction: GlobalBulletSpawnerParallalDirection = $GlobalBulletSpawnerParallalDirection
+@onready var global_bullet_spawner: GlobalBulletSpawner = $GlobalBulletSpawner
 @onready var parallal_tower: ParallalTower = $ParallalTower
 @onready var target_player_tower_left: TargetPlayerTower = $TargetPlayerTowerLeft
 @onready var target_player_tower_right: TargetPlayerTower = $TargetPlayerTowerRight
@@ -16,10 +16,10 @@ func _ready() -> void:
 
 func _to_normal() -> void:
 	super._to_normal()
-	chaser.movement_speed = 300
-	global_bullet_parallal_direction.frequency = 0.4
-	global_bullet_parallal_direction.speed = 400
-	global_bullet_target_player.frequency = 1
+	chaser.movement_speed = 320
+	global_bullet_spawner_parallal_direction.frequency = 0.45
+	global_bullet_spawner_parallal_direction.speed = 410
+	global_bullet_spawner.frequency = 1
 	parallal_tower.disabled = false
 	target_player_tower_left.disabled = false
 	target_player_tower_right.disabled = false
@@ -27,13 +27,13 @@ func _to_normal() -> void:
 func _to_hard() -> void:
 	super._to_hard()
 	chaser.movement_speed = 400
-	global_bullet_parallal_direction.frequency = 0.25
-	global_bullet_parallal_direction.speed = 450
-	global_bullet_target_player.frequency = 0.6
-	global_bullet_target_player.speed = 600
+	global_bullet_spawner_parallal_direction.frequency = 0.3
+	global_bullet_spawner_parallal_direction.speed = 450
+	global_bullet_spawner.frequency = 0.7
+	global_bullet_spawner.speed = 600
 	parallal_tower.frequency = 0.8
 	parallal_tower.speed = 500 
-	target_player_tower_left.frequency = 0.3
-	target_player_tower_right.speed = 700
+	target_player_tower_left.frequency = 0.25
+	target_player_tower_right.speed = 750
 	chaser.leavse_fire_trail = true
 	
