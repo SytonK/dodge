0cbae8e4ea6d98513ccd0cc497c455c6919d3adc
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-23 23:44:35 +0300

    use global spawnes lvl 0

--- src/levels/level 0/level_0.gd
+++ src/levels/level 0/level_0.gd
@@ -1,15 +1,14 @@
 extends Level
 
-
-@onready var global_bullet_target_player: GlobalBulletTargetPlayer = $GlobalBulletTargetPlayer
+@onready var global_bullet_spawner: GlobalBulletSpawner = $GlobalBulletSpawner
 @onready var target_player_tower_left: TargetPlayerTower = $TargetPlayerTowerLeft
 @onready var target_player_tower_right: TargetPlayerTower = $TargetPlayerTowerRight
-@onready var fast_global_bullet_target_player: GlobalBulletTargetPlayer = $FastGlobalBulletTargetPlayer
+@onready var fast_global_bullet_spawner: GlobalBulletSpawner = $FastGlobalBulletSpawner
 
 
 func _to_normal() -> void:
 	super._to_normal()
-	global_bullet_target_player.frequency = 0.35
+	global_bullet_spawner.frequency = 0.35
 	target_player_tower_left.disabled = false
 	await get_tree().create_timer(0.5).timeout
 	target_player_tower_right.disabled = false
@@ -18,5 +17,5 @@ func _to_hard() -> void:
 	super._to_hard()
 	target_player_tower_left.speed = 650
 	target_player_tower_right.speed = 650
-	global_bullet_target_player.frequency = 0.3
-	fast_global_bullet_target_player.disabled = false
+	global_bullet_spawner.frequency = 0.3
+	fast_global_bullet_spawner.disabled = false
