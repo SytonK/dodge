84d27b3d1704fb79e63ef6a32fcbf2020fddb218
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-23 21:55:26 +0300

    add invulnerable

--- src/player/player.gd
+++ src/player/player.gd
@@ -14,6 +14,11 @@ signal game_over
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@export var is_invulnerable: bool = false : set = _set_is_invulnerable
+@onready var hurtbox: Hurtbox = $Hurtbox
+@onready var polygon_2d: Polygon2D = $Polygon2D
+
+
 func _ready() -> void:
 	PlayerRef.player_ref = self
 
@@ -39,3 +44,10 @@ func _on_hurtbox_hurt() -> void:
 
 func _game_over() -> void:
 	game_over.emit()
+
+
+func _set_is_invulnerable(new_is_invulnerable: bool) -> void:
+	is_invulnerable = new_is_invulnerable
+	hurtbox.monitoring = !is_invulnerable
+	polygon_2d.color = Color(1,1,1,0.4 if is_invulnerable else 1.0)
+	
