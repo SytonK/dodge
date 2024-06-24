d1347e62470d6dcc30e099afa28d22382f7c3f93
SytonK <syton98k@gmail.com>
2024-06-24 13:55:57 +0300

    fix lava spawner

--- src/enemies attacks/lava/lava.gd
+++ src/enemies attacks/lava/lava.gd
@@ -1,24 +1,8 @@
 class_name Lava
-extends Hitbox
+extends Fire
 
 
-var radius: Vector2
-
-
-func _init(radius_arg: Vector2) -> void:
+func _init() -> void:
 	super._init()
-	radius = radius_arg
-	_init_collision_shape()
 	monitorable = false
 
-
-func _init_collision_shape() -> void:
-	var new_collision_shape = CollisionShape2D.new()
-	new_collision_shape.shape = _init_shape()
-	add_child(new_collision_shape)
-	
-
-func _init_shape() -> RectangleShape2D:
-	var new_shape = RectangleShape2D.new()
-	new_shape.size = radius
-	return new_shape
--- src/enemies/global enemies/lava spawner/lava_spawner.gd
+++ src/enemies/global enemies/lava spawner/lava_spawner.gd
@@ -2,7 +2,8 @@ class_name LavaSpawner
 extends Node2D
 
 
-@export var radius: Vector2
+@export var lava_polygon: PackedVector2Array
+@export var lava_lifetime: float
 @export var frequency: float : set = _set_frequency
 @onready var timer: Timer
 
@@ -21,7 +22,11 @@ func _init_timer() -> void:
 	add_child(timer)
 
 func _spawn_lava() -> void:
-	add_child(Lava.new(Vector2(radius)))
+	var new_lava = Lava.new()
+	new_lava.lifetime = lava_lifetime
+	new_lava.polygon_packed_vector_array = lava_polygon
+	new_lava.polygon_color = Color(0.8, 0.4, 0.1, 0.9)
+	add_child(new_lava)
 
 
 func _set_frequency(new_frequency: float) -> void:
