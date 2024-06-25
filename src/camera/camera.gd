09182bc394b41c8cf68f91f87df7b68d91f839c5
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-25 21:40:26 +0300

    rename shake weigth

--- src/camera/camera.gd
+++ src/camera/camera.gd
@@ -5,13 +5,13 @@ extends Camera2D
 
 
 @export var shake_strength: float = 10
-@export var shake_weigth: float = 5
+@export var shake_fade: float = 5
 var curr_shake_strength: float = 0
 
 
 func _process(delta: float) -> void:
 	if curr_shake_strength > 0:
-		curr_shake_strength = lerpf(curr_shake_strength, 0, shake_weigth * delta)
+		curr_shake_strength = lerpf(curr_shake_strength, 0, shake_fade * delta)
 		
 		offset = Vector2(randf_range(-curr_shake_strength,curr_shake_strength),randf_range(-curr_shake_strength,curr_shake_strength))
 
