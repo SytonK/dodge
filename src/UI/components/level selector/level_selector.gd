b921eb1a5dea1b5ca5a8fb6ab343b2b8bc7db45f
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-28 15:32:43 +0300

    add score star to level selector

--- src/UI/components/level selector/level_selector.gd
+++ src/UI/components/level selector/level_selector.gd
@@ -12,12 +12,18 @@ const MAX_LEVEL: int = 3
 var level: int = 0: set = _set_level
 @onready var change_scene_button: ChangeSceneButton = $HBoxContainer/ChangeSceneButton
 
+const EMPTY_STAR = preload("res://assets/UI/stars/empty_star.png")
+const FULL_STAR = preload("res://assets/UI/stars/full_star.png")
+@onready var score_stars: Array[TextureRect] = [$ScoreUI/Star0, $ScoreUI/Star1, $ScoreUI/Star2]
+var score: Score
 
 func _ready() -> void:
 	change_scene_button.grab_focus()
+	_init_score()
 
 func _set_level(new_level: int) -> void:
 	level = clamp(new_level, 0, MAX_LEVEL)
+	_set_stars()
 	if change_scene_button:
 		change_scene_button.text = BUTTON_TEXT + str(level)
 		change_scene_button.file_path = LEVEL_PATH0 + str(level) + LEVEL_PATH1 + str(level) + LEVEL_PATH2
@@ -28,3 +34,12 @@ func _on_button_left_pressed() -> void:
 
 func _on_button_right_pressed() -> void:
 	level += 1
+
+
+func _init_score() -> void:
+	score = Score.load()
+	_set_stars()
+
+func _set_stars() -> void:
+	for star_index in score_stars.size():
+		score_stars[star_index].texture = FULL_STAR if score.stars[level] > star_index else EMPTY_STAR
