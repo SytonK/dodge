9b0eb41e991c59ff0c3bf87b5e8e6e05507db5bc
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-06-28 16:23:05 +0300

    gain star sound

--- src/UI/level UI/stars/stars.gd
+++ src/UI/level UI/stars/stars.gd
@@ -6,7 +6,9 @@ const FULL_STAR = preload("res://assets/UI/stars/full_star.png")
 
 @onready var stars: Array[TextureRect] = [$Star0, $Star1, $Star2]
 
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
 
 func add_star(star_index: int) -> void:
 	stars[star_index].texture = FULL_STAR
-
+	audio_stream_player.play()
