79cc5af502d2ce219e8138e9d2a4b61183964184
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-12 15:09:26 +0300

    add audio reset button

---
+++ src/UI/menus/settings/audio/audio_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+@onready var master_audio_slider : AudioSlider = $VBoxContainer/Master/AudioSlider
+@onready var music_audio_slider: AudioSlider = $VBoxContainer/Music/AudioSlider
+@onready var SFX_audio_slider: AudioSlider = $VBoxContainer/SFX/AudioSlider
+
+
+func _on_reset_button_pressed() -> void:
+	master_audio_slider.value = 0
+	AudioServer.set_bus_volume_db(0, 0)
+	ConfigFileHandler.save_audio_settings('master_volum', 0)
+	
+	music_audio_slider.value = 0
+	AudioServer.set_bus_volume_db(1, 0)
+	ConfigFileHandler.save_audio_settings('music_volum', 0)
+	
+	SFX_audio_slider.value = 0
+	AudioServer.set_bus_volume_db(2, 0)
+	ConfigFileHandler.save_audio_settings('sfx_volum', 0)
