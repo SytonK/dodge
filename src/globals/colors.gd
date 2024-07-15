28275b7b95eb9fe936d81b03e8d5af936b6b6813
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-16 00:45:30 +0300

    change bullet color

--- src/globals/colors.gd
+++ src/globals/colors.gd
@@ -6,4 +6,4 @@ const MODULATE_FACTOR: float = 0.001
 const max_speed: float = 1000
 
 static func get_bullet_color(new_bullet: Bullet, bullet_color: Color) -> Color:
-	return Color(bullet_color * new_bullet.movment_speed / max_speed, 1)
+	return Color(bullet_color * (0.5 + (new_bullet.movment_speed / max_speed)), 1)
