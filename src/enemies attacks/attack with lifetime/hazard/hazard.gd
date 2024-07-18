c70b17ae969342caf45602eeef102735098f443a
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-18 09:49:58 +0300

    hazard work in progress

--- src/enemies attacks/attack with lifetime/hazard/hazard.gd
+++ src/enemies attacks/attack with lifetime/hazard/hazard.gd
@@ -1,12 +1,41 @@
 class_name Hazard
 extends LifetimeAttack
 
+
 const A_FACTOR_AT_SETUP: float = 0.4
 
+
 @export var setup_time: float
+@onready var setup_timer: Timer = $SetupTimer
+
+var at_setup: bool = true
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 
+func _ready() -> void:
+	setup_timer.wait_time = setup_time
+
 func _process(delta: float) -> void:
 	super._process(delta)
-	sprite_2d.self_modulate.a = ((A_FACTOR_AT_SETUP * curr_lifetime) / setup_time) if curr_lifetime < setup_time else 1.0
+	_calc_sprite_a()
+
+
+func _calc_sprite_a() -> void:
+	if at_setup:
+		sprite_2d.self_modulate.a = A_FACTOR_AT_SETUP * curr_lifetime / setup_time
+
+
+func _set_at_setup(new_at_setup: bool) -> void:
+	at_setup = new_at_setup
+	if !at_setup:
+		monitorable = true
+		monitoring = true
+		sprite_2d.self_modulate.a = 1
+
+
+func _on_setup_timer_timeout() -> void:
+	at_setup = true
+	monitorable = true
+	monitoring = true
+	sprite_2d.self_modulate.a = 1
