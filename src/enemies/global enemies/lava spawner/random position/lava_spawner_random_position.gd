eb49a6ba04e753fcd9609b22f1f7022bcc9aa9bc
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-18 09:22:16 +0300

    remove lava

--- src/enemies/global enemies/lava spawner/random position/lava_spawner_random_position.gd
+++
@@ -1,7 +0,0 @@
-class_name LavaSpawnerRandomPosition
-extends LavaSpawner
-
-
-
-func _get_position() -> Vector2:
-	return Screen.get_random_position()
--- src/levels/level 3/level_3.gd
+++ src/levels/level 3/level_3.gd
@@ -1,28 +1,19 @@
 extends Level
 
 
-@onready var lava_spawner: LavaSpawner = $LavaSpawner
-@onready var lava_spawner_random_position: LavaSpawnerRandomPosition = $LavaSpawnerRandomPosition
 @onready var global_bullet_spawner: GlobalBulletSpawner = $GlobalBulletSpawner
 @onready var global_bullet_spawner_parallal_direction: GlobalBulletSpawnerParallalDirection = $GlobalBulletSpawnerParallalDirection
 
 
 func _to_normal() -> void:
 	super._to_normal()
-	lava_spawner.frequency = 0.75
-	lava_spawner.lava_lifetime = 6
-	lava_spawner.lava_polygon = [Vector2(40,40), Vector2(40,-40), Vector2(-40,-40), Vector2(-40,40)]
-	lava_spawner_random_position.disabled = false
+
 	global_bullet_spawner.frequency = 0.2
 	global_bullet_spawner_parallal_direction.speed = 725
 	
 
 func _to_hard() -> void:
 	super._to_hard()
-	lava_spawner.frequency = 0.35
-	lava_spawner_random_position.lava_polygon = [Vector2(0,30), Vector2(20,20), Vector2(30,0), Vector2(20,-20)
-		,Vector2(0,-30), Vector2(-20,-20), Vector2(-30,0), Vector2(-20,20)]
-	lava_spawner_random_position.frequency = 0.5
 	global_bullet_spawner.frequency = 0.1
 	global_bullet_spawner.speed = 325
 	global_bullet_spawner_parallal_direction.speed = 850
