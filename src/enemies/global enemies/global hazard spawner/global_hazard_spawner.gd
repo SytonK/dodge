607ad55e8943161e3b63efed26a8098f2e055479
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-19 21:47:04 +0300

    allow hazard scale

--- src/enemies/global enemies/global hazard spawner/global_hazard_spawner.gd
+++ src/enemies/global enemies/global hazard spawner/global_hazard_spawner.gd
@@ -10,6 +10,8 @@ var timer: Timer
 @export var lifetime: float
 @export var setup_time: float
 
+@export var hazard_scale: Vector2 = Vector2(1,1)
+
 
 func _ready() -> void:
 	_init_timer()
@@ -28,6 +30,7 @@ func _on_timer_timeout() -> void:
 	new_hazard.lifetime = lifetime
 	new_hazard.setup_time = setup_time
 	new_hazard.position = _get_new_hazard_position()
+	new_hazard.scale = Vector2(hazard_scale)
 	add_child(new_hazard)
 
 func _get_new_hazard_position() -> Vector2:
