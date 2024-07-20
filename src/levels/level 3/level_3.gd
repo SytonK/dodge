c2a7fc07f061df20dbfca155a4d70b8f270d16b0
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-20 03:22:49 +0300

    level 3 normal mode

--- src/levels/level 3/level_3.gd
+++ src/levels/level 3/level_3.gd
@@ -1,10 +1,20 @@
 extends Level
 
 
+@onready var global_hazard_spawner: GlobalHazardSpawner = $GlobalHazardSpawner
+@onready var global_random_position_hazard_spawner: GlobalRandomPositionHazardSpawner = $GlobalRandomPositionHazardSpawner
+@onready var global_bullet_spawner: GlobalBulletSpawner = $GlobalBulletSpawner
+@onready var fast_global_bullet_spawner: GlobalBulletSpawner = $FastGlobalBulletSpawner
+@onready var global_bullet_spawner_parallal_direction: GlobalBulletSpawnerParallalDirection = $GlobalBulletSpawnerParallalDirection
 
-func _to_normal() -> void:
+
+func _to_normal() -> void: 
 	super._to_normal()
-
+	global_hazard_spawner.hazard_scale = Vector2(1.25, 1.25)
+	global_random_position_hazard_spawner.disabled = false
+	global_bullet_spawner.frequency = 0.4
+	fast_global_bullet_spawner.disabled = false
+	global_bullet_spawner_parallal_direction.frequency = 0.3
 
 func _to_hard() -> void:
 	super._to_hard()
