3e3ec6a0ba6ef8739369480d7666d4f393395653
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-22 01:20:38 +0300

    noen line / base

---
+++ src/levels/envierment/path follower/trail/trail.gd
@@ -0,0 +1,26 @@
+class_name Trail
+extends Line2D
+
+
+@export var max_length: int
+var queue : Array
+
+ 
+func _process(_delta: float) -> void:
+	_update_points()
+
+func _update_points() -> void:
+	var pos = _get_position()
+	
+	queue.push_front(pos)
+	
+	if  queue.size() > max_length:
+		queue.pop_back()
+	
+	clear_points()
+	
+	for point in queue:
+		add_point(point)
+
+func _get_position() -> Vector2:
+	return owner.position
