727a993df26bd079b95677d7c6ee25b5c760f113
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-25 19:09:50 +0300

    ui background

--- src/levels/envierment/path follower/path_follower.gd
+++ src/levels/envierment/path follower/path_follower.gd
@@ -5,17 +5,19 @@ extends PathFollow2D
 @export var speed: float
 @export var gradiant: Gradient : set = _set_gradiant
 
+@export var max_length: int = 60
 @onready var trail: Trail = $Trail
 
 
 func _ready() -> void:
 	_set_gradiant(gradiant)
+	trail.max_length = max_length
 
 func _process(delta: float) -> void:
 	progress_ratio += speed * delta
 
 
 func _set_gradiant(new_gradiant: Gradient) -> void:
+	gradiant = new_gradiant
 	if trail:
-		gradiant = new_gradiant
 		trail.gradient = gradiant
