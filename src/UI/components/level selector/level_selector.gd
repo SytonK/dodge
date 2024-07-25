9181452d8469e2e5b61e4ea94b77e78c5bb22e5e
SytonK <syton98k@gmail.com>
2024-07-25 11:45:45 +0300

    fix change buttons name

--- src/UI/components/level selector/level_selector.gd
+++ src/UI/components/level selector/level_selector.gd
@@ -10,7 +10,7 @@ const LEVEL_PATH2: String = '.tscn'
 const MAX_LEVEL: int = 3
 
 var level: int = 0: set = _set_level
-@onready var change_scene_button: ChangeSceneButton = $HBoxContainer/ChangeSceneButton
+@onready var select_level_button: ChangeSceneButton = $HBoxContainer/SelectLevelButton
 
 const EMPTY_STAR = preload("res://assets/UI/stars/empty_star.png")
 const FULL_STAR = preload("res://assets/UI/stars/full_star.png")
@@ -18,15 +18,15 @@ const FULL_STAR = preload("res://assets/UI/stars/full_star.png")
 var score: Score
 
 func _ready() -> void:
-	change_scene_button.grab_focus()
+	select_level_button.grab_focus()
 	_init_score()
 
 func _set_level(new_level: int) -> void:
 	level = clamp(new_level, 0, MAX_LEVEL)
 	_set_stars()
-	if change_scene_button:
-		change_scene_button.text = BUTTON_TEXT + str(level)
-		change_scene_button.file_path = LEVEL_PATH0 + str(level) + LEVEL_PATH1 + str(level) + LEVEL_PATH2
+	if select_level_button:
+		select_level_button.text = BUTTON_TEXT + str(level)
+		select_level_button.file_path = LEVEL_PATH0 + str(level) + LEVEL_PATH1 + str(level) + LEVEL_PATH2
 
 
 func _on_button_left_pressed() -> void:
