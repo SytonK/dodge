406319c839441484fbcb17338a6c7e2dfc650ff7
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-25 19:57:40 +0300

    main menu/add background

---
+++ src/UI/menus/main menu/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+@onready var game_title: Label = $GameTitle
+@onready var main_menu_v_box: VBoxContainer = $MainMenuVBox
+
+@onready var settings_menu: Control = $SettingsMenu
+
+
+func _on_settings_menu_pressed() -> void:
+	game_title.visible = false
+	main_menu_v_box.visible = false
+	settings_menu.visible = true
+
+
+func _on_main_menu_button_pressed() -> void:
+	game_title.visible = true
+	main_menu_v_box.visible = true
+	settings_menu.visible = false
