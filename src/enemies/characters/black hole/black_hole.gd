25ee9470a431618579e69b7de41c0b5981e0bb8c
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-31 12:49:58 +0300

    BlackHole/animation

--- src/enemies/characters/black hole/black_hole.gd
+++ src/enemies/characters/black hole/black_hole.gd
@@ -2,10 +2,9 @@ class_name BlackHole
 extends Hurtbox
 
 
-const rotation_speed_1: float = 2
-const rotation_speed_2: float = 2
-const rotation_speed_3: float = 2
-const rotation_speed_4: float = 2
+const rotation_speed_1: float = 1
+const rotation_speed_2: float = -.8
+const rotation_speed_4: float = 0.4
 
 
 @onready var black_hole_0: Sprite2D = $BlackHole0
@@ -17,6 +16,6 @@ const rotation_speed_4: float = 2
 
 func _process(delta: float) -> void:
 	black_hole_1.rotation += rotation_speed_1 * delta
-	black_hole_2.rotation += rotation_speed_1 * delta
-	black_hole_3.rotation += rotation_speed_1 * delta
-	black_hole_4.rotation += rotation_speed_1 * delta
+	black_hole_2.rotation += rotation_speed_2 * delta
+	black_hole_3.rotation += rotation_speed_2 * delta
+	black_hole_4.rotation += rotation_speed_4 * delta
