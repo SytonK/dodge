49c75c0a19a0d4712aa953b7f9cdbde80502cda3
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-08-03 22:47:50 +0300

    Level5/normal

--- src/levels/level 5/level_5.gd
+++ src/levels/level 5/level_5.gd
@@ -1,6 +1,29 @@
 extends Level
 
 
+@onready var global_bullet_spawner: GlobalBulletSpawner = $GlobalBulletSpawner
+@onready var global_bullet_spawner_parallal_direction: GlobalBulletSpawnerParallalDirection = $GlobalBulletSpawnerParallalDirection
+@onready var parallal_tower: ParallalTower = $ParallalTower
+@onready var target_player_tower_top_rigth: TargetPlayerTower = $TargetPlayerTowerTopRigth
+@onready var target_player_tower_bottom_rigth: TargetPlayerTower = $TargetPlayerTowerBottomRigth
+@onready var target_player_tower_bottom_left: TargetPlayerTower = $TargetPlayerTowerBottomLeft
+@onready var target_player_tower_top_left: TargetPlayerTower = $TargetPlayerTowerTopLeft
+
+
 func _ready() -> void:
 	super._ready()
 	player.position = Vector2(0, 150)
+
+func _to_normal() -> void: 
+	super._to_normal()
+	global_bullet_spawner.speed = 700
+	global_bullet_spawner_parallal_direction.frequency = .4
+	parallal_tower.frequency = 1
+	target_player_tower_top_rigth.disabled = false
+	await get_tree().create_timer(.8).timeout
+	target_player_tower_bottom_rigth.disabled = false
+	await get_tree().create_timer(.8).timeout
+	target_player_tower_bottom_left.disabled = false
+	await get_tree().create_timer(.8).timeout
+	target_player_tower_top_left.disabled = false
+	
