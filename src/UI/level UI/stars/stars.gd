bdd12937687251e2aed80c9097c58a6abaebb1a8
SytonK <syton98k@gmail.com>
2024-08-05 11:42:21 +0300

    Stars/change color

--- src/UI/level UI/stars/stars.gd
+++ src/UI/level UI/stars/stars.gd
@@ -11,4 +11,5 @@ const FULL_STAR = preload("res://assets/UI/stars/full_star.png")
 
 func add_star(star_index: int) -> void:
 	stars[star_index].texture = FULL_STAR
+	stars[star_index].self_modulate = Color(1,1,1)
 	audio_stream_player.play()
